0af0afadd1edd3e573cf7c5b4ee25c5ccba49360
larbi <99117566+larbi1512@users.noreply.github.com>
2023-09-28 17:13:52 +0100

    shop page, do not merge Ig

---
+++ src/components/shop/Body.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Filter from "./Filter";
+const Body=()=>{
+    return (
+   <div>
+    <div className="flex flex-row">
+        <Filter/>
+        <p>hello</p>
+
+    </div>
+   </div>
+    )
+}
+
+export default Body;
--- src/views/shop/shopItemsSorted.jsx
+++ src/views/shop/shopItemsSorted.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import navbar from "../../components/navbar"
-import Body from "../../components/shop/Hero"
+import Hero from "../../components/shop/Hero"
 import Footer from "../../components/Footer";
+import Body from "../../components/shop/Body";
     
 
 const Shop = () => {
     return (
         <div className="overflow-hidden">
             <navbar/>
+            <Hero/>
             <Body/>
             <Footer/>
         </div>
