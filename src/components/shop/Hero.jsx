df242ac639d132c354fdb4344d0aa10323c5f2b2
Sarra-arab <sarra.arab@ensia.edu.dz>
2023-10-23 11:44:17 +0100

    update hero.jsx shop

--- src/components/shop/Hero.jsx
+++ src/components/shop/Hero.jsx
@@ -15,63 +15,32 @@ const Hero = () => {
     },
   ];
 
-
-    return (
-        <div className="w-full mb-4">
-            <div className="flex flex-col items-center justify-center w-full px-[10px] lg:px-[50px]">
-                <p className="font-sofia text-center text-[24px] md:text-[42px] lg:text-[42px] mt-4 font-normal">
-                    Small business of the week
-                </p>
-                {Data.map((item, index) => (
-                    <p
-                        key={index}
-                        className="font-sunflower text-[#FF9494] font-medium text-[24px] md:text-[42px] lg:text-[42px]"
-                    >
-                        {item.title}
-                    </p>
-                ))}
-                <div className="flex flex-row items-center justify-center gap-[15px] lg:gap-[30px]  ">
-                    <div className="flex flex-col items-center  " style={{ flex: "0.5" }}>
-                            <img src={Data[0].images[0]}
-                                alt="Image"
-                            className=""
-                            />
-                        <img
-                            src={Data[0].images[3]}
-                            alt="Image"
-                            className=""
-
-                        />
-                    </div>
-                    <div className="flex flex-col items-center " style={{ flex: "0.5" }}>
-                        <img
-                            src={Data[0].images[1]}
-                            alt="Image 1"
-                        />
-                    </div>
-                    <div className="flex flex-col items-center  " style={{ flex: "0.5" }}>
-                        <img
-                            src={Data[0].images[2]}
-                            alt="Image"
-                            className=""
-                        />
-                        <img
-                            src={Data[0].images[4]}
-                            alt="Image"
-                            className=""
-
-                        />
-                    </div>
-                </div>
-                {Data.map((item, index) => (
-                    <p
-                        key={index}
-                        className="font-sunflower px-0 md:px-20 lg:px-[100px] text-center text-black font-normal mt-[20px] text-[16px] md:text-[24px] lg:text-[32px]"
-                    >
-                        {item.description}
-                    </p>
-                ))}
-            </div>
+  return (
+    <div className="w-full mb-4">
+      <div className="flex flex-col items-center justify-center w-full px-[10px] lg:px-[50px]">
+        <p className="font-sofia text-center text-[24px] md:text-[42px] lg:text-[42px] mt-4 font-normal">
+          Small business of the week
+        </p>
+        {Data.map((item, index) => (
+          <p
+            key={index}
+            className="font-sunflower text-[#FF9494] font-medium text-[24px] md:text-[42px] lg:text-[42px]"
+          >
+            {item.title}
+          </p>
+        ))}
+        <div className="flex flex-row items-center justify-center gap-[15px] lg:gap-[30px]">
+          <div className="flex flex-col items-center" style={{ flex: "0.5" }}>
+            <img src={Data[0].images[0]} alt="Image" className="" />
+            <img src={Data[0].images[3]} alt="Image" className="" />
+          </div>
+          <div className="flex flex-col items-center" style={{ flex: "0.5" }}>
+            <img src={Data[0].images[1]} alt="Image 1" />
+          </div>
+          <div className="flex flex-col items-center" style={{ flex: "0.5" }}>
+            <img src={Data[0].images[2]} alt="Image" className="" />
+            <img src={Data[0].images[4]} alt="Image" className="" />
+          </div>
         </div>
         {Data.map((item, index) => (
           <p
