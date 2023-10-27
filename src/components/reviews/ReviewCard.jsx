92df58507f53cc73393c1c0c47b26ab0d6ecf75d
AbderrahimRezki <abderrahim4cem@gmail.com>
2023-10-27 18:00:16 +0100

    added background / truncate text if long

--- src/components/reviews/ReviewCard.jsx
+++ src/components/reviews/ReviewCard.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Stars from "../Stars";
+
+// css
+import "./review-card.css";
+
+const textSizes = {
+  600: 162,
+  768: 300,
+  1200: 162,
+  1536: 300,
+  1920: 500,
+};
 
 const ReviewCard = ({
   title = "Green Poetry Set 4 argile pieces 1lb",
@@ -7,11 +18,38 @@ const ReviewCard = ({
   seller = "PottyPot",
   price = 1000,
   stars = 4,
-  description = "Et est quae aspernatur commodi officiis sit consectetur sint maxime. Nisi et quis aperiam commodi quis accusamus ipsam. Aut suscipit adipisci quisquam quasi quasi laboriosam explicabo voluptatem. Aliquam illum qui voluptate consequatur maxime a ullam. Eveniet eum nihil possimus sint et numquam. Voluptas quia rem sit.",
+  description = "Et est quae aspernatur commodi officiis sit consectetur sint maxime. Nisi et quis aperiam commodi quis accusamus ipsam. Aut suscipit adipisci quisquam quasi quasi laboriosam explicabo voluptatem. Aliquam illum qui voluptate consequatur maxime a ullam. Eveniet eum nihil possimus sint et numquam. Voluptas quia rem sit.Et est quae aspernatur commodi officiis sit consectetur sint maxime. Nisi et quis aperiam commodi quis accusamus ipsam. Aut suscipit adipisci quisquam quasi quasi laboriosam explicabo voluptatem. Aliquam illum qui voluptate consequatur maxime a ullam. Eveniet eum nihil possimus sint et numquam. Voluptas quia rem sit.Et est quae aspernatur commodi officiis sit consectetur sint maxime. Nisi et quis aperiam commodi quis accusamus ipsam. Aut suscipit adipisci quisquam quasi quasi laboriosam explicabo voluptatem. Aliquam illum qui voluptate consequatur maxime a ullam. Eveniet eum nihil possimus sint et numquam. Voluptas quia rem sit.",
   date = new Date(Date.now()).toLocaleDateString("fr-FR"),
 }) => {
+
+  const [size, setSize] = useState();
+
+  const getClosestBreakpoint = () => {
+    for (const breakpoint in textSizes)
+      if (window.innerWidth < breakpoint) 
+        return breakpoint
+    return 1920
+  }
+
+  const handleResize = () => setSize(textSizes[getClosestBreakpoint()])
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener("resize", () => {
+      console.log(window.innerWidth);
+      handleResize();
+    });
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="bg-[#FDDED2] rounded-2xl overflow-hidden flex flex-col max-w-[80%] md:flex-row lg:flex-col xl:flex-row xl:max-w-[100%]">
+    <div
+      className="rc
+    bg-[#FDDED2] rounded-2xl overflow-hidden 
+    flex flex-col max-w-[80%] md:flex-row lg:flex-col xl:flex-row xl:max-w-[100%]"
+    >
       <img
         src={image}
         alt="product"
@@ -31,7 +69,11 @@ const ReviewCard = ({
         </div>
         <div>
           <Stars average={stars} totalNumberOfStars={5} />
-          <p className="bg-white py-3 px-2 my-2 rounded-md">{description}</p>
+          <p className="bg-white py-3 px-2 my-2 rounded-md">
+            {description.length > size
+              ? description.slice(0, size) + "..."
+              : description}
+          </p>
         </div>
         <div className="grid w-full">
           <span className="justify-self-end text-black/[.55]">{date}</span>
