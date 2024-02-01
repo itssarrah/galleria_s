09166e66820b9f92cb278c04dc4088605e418c21
Sarra-arab <sarra.arab@ensia.edu.dz>
2024-02-01 23:09:13 +0100

    vite merged

---
+++ src/components/Landing/TrendingShops.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import ShopCardSlider from "./ShopCardSlider";
+import { useQuery } from "react-query";
+import { BACKEND_URL } from "../../config";
+
+function TrendingShops(props) {
+  const { t } = useTranslation("homepage");
+
+  const {
+    data: topCategoriesAndBusinesses,
+    isLoading,
+    isError,
+  } = useQuery("topCategoriesAndBusinesses", async () => {
+    const response = await fetch(
+      `${BACKEND_URL}api/top-categories-and-businesses`
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError || !topCategoriesAndBusinesses) {
+    return <div>Error loading data</div>;
+  }
+
+  return (
+    <div {...props}>
+      <div className="pb-12">
+        <h1 className="primary_txt">{t("shop_header")}</h1>
+        <h2 className="secondary_txt">{t("shop_subheader")}</h2>
+      </div>
+      {topCategoriesAndBusinesses.data.map((result) => (
+        <div>
+          <h1 className="category_name relative text-base sm:text-lg md:text-xl lg:text-2xl pb-4">
+            {t("category_production")} {result.category.en_name}
+          </h1>
+          <ShopCardSlider businesses={result.businesses} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TrendingShops;
---
+++ src/components/shop/Format.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "../../css/MultiRangeSlider.css";
+
+function Format({ type, formData, setFormData }) {
+  const handleFormatChange = (e) => {
+    setFormData({
+      ...formData,
+      selectedFormat: e.target.value,
+    });
+    console.log(formData);
+  };
+  return (
+    <div className={`${type == "false" ? "hidden" : "flex"} flex-col gap-3`}>
+      <p className="text-black font-sofia text-[22px] text-left">Format :</p>
+      <div className="flex flex-row gap-[8px]">
+        <input
+          className="relative float-left ml-[1.5rem] mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-[rgb(255,148,148)] before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-[#FF9494] dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary  dark:checked:focus:border-primary dark:checked:focus:before:shadow-[0px_0px_0px_13px_#FF9494]"
+          type="radio"
+          id="small business"
+          name="format"
+          value="Small business"
+          onChange={handleFormatChange}
+          checked={formData.selectedFormat === "Small business"}
+        />
+        <label>
+          <span className="text-black text-opacity-[70%] font-sunflower text-[18px] text-left">
+            Small business
+          </span>
+        </label>
+      </div>
+      <div className="flex flex-row gap-[8px] mt-2">
+        <input
+          className="relative float-left ml-[1.5rem] mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-[rgb(255,148,148)] before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-[#FF9494] dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary  dark:checked:focus:border-primary dark:checked:focus:before:shadow-[0px_0px_0px_13px_#FF9494]"
+          type="radio"
+          id="items"
+          name="format"
+          value="items/products"
+          onChange={handleFormatChange}
+          checked={formData.selectedFormat === "items/products"}
+        />
+        <label>
+          <span className="text-black text-opacity-[70%] font-sunflower text-[18px] text-left">
+            Items / Products
+          </span>
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default Format;
