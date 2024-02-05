cba7ddb83a52aabef7ef5a12b88905d806409f97
Sarra-arab <sarra.arab@ensia.edu.dz>
2024-02-05 13:04:15 +0100

    idk

--- src/components/auth/businessauth.jsx
+++ src/components/auth/businessauth.jsx
@@ -110,9 +110,13 @@ const Businessauth = () => {
         `${BACKEND_URL}api/check-email?email=${formData.email}`
       );
 
+      console.log(response.data);
+      console.log(response);
+
       if (response.data.exists) {
         errorList.email = t("email_already_exists_error");
       }
+      console.log("checked email");
     } catch (error) {
       console.error("Error checking email:", error);
       errorList.email = t("error_checking_email");
